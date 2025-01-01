6426983241c60462dcd3b28d7652bbe076662d9d
Ilhom Maksadkulov <ilhommaksadkulov@Ilhoms-MacBook-Pro.local>
2025-01-01 18:08:34 +0500

    feat: added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,9 +26,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-5`}
       >
-        {children}
+        <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+          <div className="flex flex-grow">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="ml-5 font-light">
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
       </body>
     </html>
   );
