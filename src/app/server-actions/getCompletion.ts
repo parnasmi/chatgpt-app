1133231e06ff694eba8f3482015dc261382fc0e3
IlhomMax <parnasmi@gmail.com>
2025-01-03 14:47:42 +0500

    feat/Store-Chat-Data-in-a-Database

--- src/app/server-actions/getCompletion.ts
+++ src/app/server-actions/getCompletion.ts
@@ -1,19 +1,24 @@
 "use server";
 import OpenAI from "openai";
 
+import { createChat, updateChat } from "@/db";
+
+import { getServerSession } from "next-auth";
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-console.log("open ai key", process.env.OPENAI_API_KEY);
 
 type MessageHistoryItem = {
   role: "user" | "assistant";
   content: string;
 };
 
-export async function getCompletion(messageHistory: MessageHistoryItem[]) {
-  // function implementation will be here
+export async function getCompletion(
+  id: number | null,
+  messageHistory: MessageHistoryItem[]
+) {
+  const session = await getServerSession();
 
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
@@ -28,5 +33,19 @@ export async function getCompletion(messageHistory: MessageHistoryItem[]) {
     },
   ];
 
-  return { messages };
+  let chatId = id;
+  if (!chatId) {
+    chatId = await createChat(
+      session?.user?.email,
+      messageHistory[0].content,
+      messages
+    );
+  } else {
+    await updateChat(chatId, messages);
+  }
+
+  return {
+    messages,
+    id: chatId,
+  };
 }
